e5a4efdc86688d2649291f8da3f3badccd685433
hheennrryy1 <hheennrryy1@icloud.com>
2016-03-20 14:33:42 +0800

    update

---
+++ src/test/java/com/henry/test/TestUserService.java
@@ -0,0 +1,24 @@
+package com.henry.test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.henry.service.UserService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:Spring.xml", "classpath:Spring-Hibernate.xml"})
+public class TestUserService {
+		
+	 	@Autowired
+	 	private UserService userService;
+	 	
+	 	@Test
+	 	public void testSave() {
+	 		String userName = "user1";
+	 		String userPassword = "123";
+	 		boolean flag = userService.findUser(userName, userPassword);
+	 		System.out.println(flag);
+	 	}
+}
